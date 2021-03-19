2cfba564cd282350ebe453b5f28988a0b4e267b5
lisha-jain <56110493+lisha-jain@users.noreply.github.com>
2021-03-19 08:21:00 +0530

    Add files via upload

---
+++ C# basics and fundamentals/VariablesAndConstants1/VariablesAndConstants1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UsingConst
+{
+    class Program
+    {
+        const int a = 10;
+
+        static void Main(string[] args)
+        {
+            const int b = 20;
+            const int c = b + a;
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C# basics and fundamentals/WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* var i = 0;
+            while (i <= 10)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine();
+                i++;
+            }*/
+           while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+                    break;
+                
+            }
+        }
+    }
+}
