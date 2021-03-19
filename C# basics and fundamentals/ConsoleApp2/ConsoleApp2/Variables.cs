5763a0e795317eaf556435044d38046b2ac98ba2
lisha-jain <56110493+lisha-jain@users.noreply.github.com>
2021-03-19 08:10:25 +0530

    Add files via upload

---
+++ C# basics and fundamentals/ConsoleApp2/ConsoleApp2/Variables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Lisha";
+            var isWorking = false;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}
