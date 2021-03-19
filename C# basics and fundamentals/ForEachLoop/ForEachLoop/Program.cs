a0919086c97ebcd305da5e2fc02f747786ae96e0
lisha-jain <56110493+lisha-jain@users.noreply.github.com>
2021-03-19 08:16:29 +0530

    Add files via upload

---
+++ C# basics and fundamentals/ForEachLoop/ForEachLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ForEachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var name = " Lisha Jain";
+            /*for ( var i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }*/
+
+            /*foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }*/
+            var numbers = new int[] { 1, 2, 3, 4, };
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ C# basics and fundamentals/Introduction to CSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using System;
+
+class Program
+{
+        static void Main()
+        {
+        Console.WriteLine("Please enter a number");
+        int  UserName = int.Parse(Console.ReadLine());
+
+        if(UserName == 1)
+        {
+            Console.WriteLine("your number is one");
+        }
+        if (UserName == 2)
+        {
+            Console.WriteLine("your number is two");
+        }
+    }
+    }
+
