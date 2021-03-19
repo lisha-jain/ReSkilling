eaa5a49d449defb089e394ffe206e663e5bcd356
lisha-jain <56110493+lisha-jain@users.noreply.github.com>
2021-03-19 08:33:33 +0530

    Add files via upload

---
+++ C# basics and fundamentals/SwitchStatement/SwitchStatement/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DemoApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 value = 11;
+            switch (value)
+            {
+                case 1:
+                    Console.WriteLine("Value is 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Value is 2");
+                    break;
+                default:
+                    Console.WriteLine("value is different");
+                    break;
+            }
+        }
+    }
+}
