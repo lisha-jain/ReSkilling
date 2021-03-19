4dd3dae8dcc4d9363818a4613b5c532914b38dfe
lisha-jain <56110493+lisha-jain@users.noreply.github.com>
2021-03-19 08:12:47 +0530

    Add files via upload

---
+++ C# basics and fundamentals/ConsoleApp3/ConsoleApp3/TypeConversion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            byte b = (byte)i;
+            var number = " 1234";
+            int j = Convert.ToInt32(number);
+            string str = "true";
+            bool b1 = Convert.ToBoolean(str);
+            Console.WriteLine(b1);
+            Console.WriteLine(j);
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ C# basics and fundamentals/ConsoleApp4/ConsoleApp4/Operators.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 20;
+            var c = 30;
+            Console.WriteLine(a + b);
+            Console.WriteLine((float)a + (float)b);
+            Console.WriteLine(a + b * c);
+            Console.WriteLine(a > b);
+            Console.WriteLine(!(a != b));
+        }
+    }
+}
